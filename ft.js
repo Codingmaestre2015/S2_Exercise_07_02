6db20d1485902dfb63b5095f658ec510455ad4be
Patrick Clawson <patrick.clawson@west-mec.org>
2018-01-17 17:57:56 -0700

    Initial Code Commit
    All student files for Exercise 07_02

---
+++ ft.js
@@ -0,0 +1,10 @@
+/*
+
+        * JavaScript: Exercise 07_02
+        * Fan Trick Fine Art Photography
+        * Digital 101 web page
+        * Author:
+        * Date:
+
+         * Filename: ft.js
+ */
