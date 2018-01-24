b1eba588a6c38e30511843bcbb96591a641172cf
Patrick Clawson <patrick.clawson@west-mec.org>
2018-01-24 16:46:14 -0700

    Updates

--- ft.js
+++ ft.js
@@ -14,6 +14,15 @@ var totalCost = 0;
 var memoryBook = false;
 var reproductionRights = false;
 
+// calculates all costs based on staff and adds to total cost
+function calcStaff() {
+        var num = document.getElementById("photognum");
+        var hrs = document.getElementById("photoghrs");
+        totalCost -= photographerCost;
+        photographerCost = num.value * 100 * hrs.value;
+        totalCost += photographerCost;
+        document.getElementById("estimate").innerHTML = "$" + totalCost;
+}
 
  // sets all form field to defaults
 function resetForm() {
@@ -22,7 +31,14 @@ function resetForm() {
         document.getElementById("membook").checked = memoryBook;
         document.getElementById("reprodrights").checked = reproductionRights;
         document.getElementById("distance").value = 0;
+calcStaff();
+createEventListeners();
+}
 
+// creates event listeners
+function createEventListeners(){
+        document.getElementById("photognum").addEventListener("change", calcStaff, false);
+        document.getElementById("photoghrs").addEventListener("change", calcStaff, false);
 }
 // resets form when page is reloaded
 window.addEventListener("load", resetForm, false);
